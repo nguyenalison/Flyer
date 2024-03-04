0865fb21cb54ecc3247364a17e85889bc3523442
nguyenalison <nguyenqalison@gmail.com>
2024-03-04 13:57:15 -0800

    Create account onCreate alter

--- login.js
+++ login.js
@@ -1,21 +1,32 @@
-let createAccountForm = document.getElementById("create-account-form");
+const createAccountButton = document.getElementById("create-account-login");
+const createAccountForm = document.getElementById("create-account-form");
 
 createAccountForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const email =  createAccountForm.querySelector("#email").value;
+  const password = createAccountForm.querySelector("#pass").value;
+  alert("Creating account...")
+  console.log("Email: " + email + "\nPassword: " + password)
+})
 
-  let email = document.getElementById("email");
-  let password = document.getElementById("pass");
+// let createAccountForm = document.getElementById("create-account-form");
 
-  if (email.value == "" || password.value == "") {
-    alert("Ensure you input a value in both fields!");
-  } else {
-    // perform operation with form input
-    alert("This form has been successfully submitted!");
-    console.log(
-      `This form has a username of ${email.value} and password of ${password.value}`
-    );
+// createAccountForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-    email.value = "";
-    password.value = "";
-  }
-});
+//   let email = document.getElementById("email");
+//   let password = document.getElementById("pass");
+
+//   if (email.value == "" || password.value == "") {
+//     alert("Ensure you input a value in both fields!");
+//   } else {
+//     // perform operation with form input
+//     alert("This form has been successfully submitted!");
+//     console.log(
+//       `This form has a username of ${email.value} and password of ${password.value}`
+//     );
+
+//     email.value = "";
+//     password.value = "";
+//   }
+// });
