1051a0a2d32989f7f2a5e8f47981393453164532
nguyenalison <nguyenqalison@gmail.com>
2024-03-04 19:39:32 -0800

    added gif, linked calendar page

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,7 +1,4 @@
-
-
 let today = new Date();
 let this_month = today.getMonth();
 const month_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
 document.getElementById("this-month").textContent = month_list[this_month];
