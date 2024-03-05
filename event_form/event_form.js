741b43983d20812bb9299b17b0048f7c0ef101d6
nguyenalison <nguyenqalison@gmail.com>
2024-03-05 12:55:27 -0800

    form alert and video added

---
+++ event_form/event_form.js
@@ -0,0 +1,18 @@
+let event_form = document.getElementById("new-event-form");
+
+event_form.addEventListener("submit", (e) =>{
+    let title = document.getElementById("title").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let notes = document.getElementById("notes").value;
+
+    if (title.value === "" || name.value === "" || email.value === "") {
+        alert("Ensure you input a in required fields!");
+    }else{
+        alert("new event created:\n" 
+        + "\ntitle: " + title
+        + "\nname: " + name 
+        + "\nemail: " + email 
+        + "\nnotes: " + notes )
+    }
+})
